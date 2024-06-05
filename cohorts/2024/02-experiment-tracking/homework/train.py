83975cc0af2c20735aed31cbd766c93e2ba07aa8
Isabelle M. Vea <isabelle.vea@gmail.com>
2024-06-05 11:34:08 -0500

    cohort 2024 02- dataprep and edits train.py
    ran data_preparation.py to create pkl prepared data, edits train.py yo add mlflow

--- cohorts/2024/02-experiment-tracking/homework/train.py
+++ cohorts/2024/02-experiment-tracking/homework/train.py
@@ -3,8 +3,13 @@ import pickle
 import click
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
+import mlflow
+
+#setting the tracking
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("green-taxi-experiment")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -22,12 +27,32 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+    with mlflow.start_run():
+        mlflow.set_tag("developer", "isabelle")
+        mlflow.log_param("train-data-path", "./output/train.pkl")
+        mlflow.log_param("valid-data-path", "./output/val.pkl")
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        mlflow.log_param("model", rf)
+
+        mlflow.log_param("max_depth", 10)
+        min_samples_split = rf.get_params()['min_samples_split']
+        mlflow.log_param("min_samples_split", min_samples_split)
+
+        rmse = root_mean_squared_error(y_val, y_pred)
+        mlflow.log_metric("rmse", rmse)
+
+
+        model_path = "models/rf_model.pkl"
+        with open(model_path, "wb") as f_out:
+            pickle.dump(rf, f_out)
+
+        mlflow.log_artifact(local_path=model_path, artifact_path="models_pickle")
 
 
 if __name__ == '__main__':
+    
     run_train()
