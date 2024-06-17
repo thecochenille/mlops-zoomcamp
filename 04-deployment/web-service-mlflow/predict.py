830846f03e03b4e2b8a04c0ce67066b1d8f4e1ce
Isabelle M. Vea <isabelle.vea@gmail.com>
2024-06-17 15:20:03 -0500

    course 04 files update from watching videos

--- 04-deployment/web-service-mlflow/predict.py
+++ 04-deployment/web-service-mlflow/predict.py
@@ -2,13 +2,19 @@ import os
 import pickle
 
 import mlflow
+from mlflow.tracking import MlflowClient
+
 from flask import Flask, request, jsonify
 
+#MLFLOW_TRACKING_URI ='http://127.0.0.1:5000'
+#mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 RUN_ID = os.getenv('RUN_ID')
-
+#RUN_ID ='6b736fe6c66e495e88a45b58a64eee67'
 logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
-# logged_model = f'runs:/{RUN_ID}/model'
+#logged_model = f'runs:/{RUN_ID}/model'
+
+
 model = mlflow.pyfunc.load_model(logged_model)
 
 
